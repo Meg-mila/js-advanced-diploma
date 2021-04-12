427e1ee3638c64e1f0d36ab97373ae59d6fa569b
Margarita Perepecho <meg.mila27@gmail.com>
2021-04-12 18:36:21 +0100

    add getPositionUser and getPositionBot

--- src/js/GameController.js
+++ src/js/GameController.js
@@ -1,15 +1,70 @@
 import themes from './themes';
+import PositionedCharacter from './PositionedCharacter';
+import Magician from './Magician';
+import Bowman from './Bowman';
+import Vampire from './Vampire';
+import Daemon from './Daemon';
+import Swordsman from './Swordsman';
+import Undead from './Undead';
+import { generateTeam } from './generators';
 
 export default class GameController {
   constructor(gamePlay, stateService) {
     this.gamePlay = gamePlay;
     this.stateService = stateService;
+    this.boardSize = gamePlay.boardSize;
   }
 
   init() {
     this.gamePlay.drawUi(themes.prairie);
+    console.log(`${'размер поля' + ': '}${this.boardSize}`);
+    const userTeam = generateTeam([Magician, Bowman, Swordsman], 4, 2);
+    const botTeam = generateTeam([Vampire, Daemon, Undead], 4, 2);
+    console.log(this.getPositionUser(), this.getPositionBot());
+    const positions = [];
+
+    for (let i = 0; i < userTeam.members.length; i += 1) {
+      const positionedUserChar = new PositionedCharacter(userTeam.members[i], this.getPositionUser());
+      if (positionedUserChar.position !== this.getPositionUser()) {
+        positions.push(positionedUserChar);
+      } else {
+        return this.getPositionUser();
+      }
+    }
+
+    for (let i = 0; i < botTeam.members.length; i += 1) {
+      const positionedBotChar = new PositionedCharacter(botTeam.members[i], this.getPositionBot());
+      if (positionedBotChar.position !== this.getPositionBot()) {
+        positions.push(positionedBotChar);
+      } else {
+        return this.getPositionBot();
+      }
+    }
+
+    console.log('fff', positions);
+
+    this.gamePlay.redrawPositions(positions);
+    console.log(userTeam.members, botTeam.members);
     // TODO: add event listeners to gamePlay events
     // TODO: load saved stated from stateService
+  }
+
+  getPositionUser() {
+    const board = this.boardSize ** 2;
+    const index = Math.floor(Math.random() * board);
+    if (index % this.boardSize === 0 || index % this.boardSize === 1) {
+      return index;
+    }
+    return this.getPositionUser();
+  }
+
+  getPositionBot() {
+    const board = this.boardSize ** 2;
+    const index = Math.floor(Math.random() * board);
+    if ((index + 1) % this.boardSize === 0 || (index + 1) % this.boardSize === (this.boardSize - 1)) {
+      return index;
+    }
+    return this.getPositionBot();
   }
 
   onCellClick(index) {
--- src/js/generators.js
+++ src/js/generators.js
@@ -1,3 +1,5 @@
+import Team from './Team';
+
 /**
  * Generates random characters
  *
@@ -5,10 +7,21 @@
  * @param maxLevel max character level
  * @returns Character type children (ex. Magician, Bowman, etc)
  */
+
 export function* characterGenerator(allowedTypes, maxLevel) {
   // TODO: write logic here
+  const randomTypes = Math.floor(Math.random() * allowedTypes.length);
+  const level = Math.floor(1 + Math.random() * maxLevel);
+  yield new allowedTypes[randomTypes](level);
 }
 
 export function generateTeam(allowedTypes, maxLevel, characterCount) {
   // TODO: write logic here
+  const team = new Team();
+  for (let i = 0; i < characterCount; i += 1) {
+    console.log('генерирую команду');
+    const randChar = characterGenerator(allowedTypes, maxLevel).next().value;
+    team.add(randChar);
+  }
+  return team;
 }
