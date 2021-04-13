5d96df2ab12258534ed70c36fdfa44918ed79a83
Margarita Perepecho <meg.mila27@gmail.com>
2021-04-13 18:50:52 +0100

    add onCellClick

--- src/js/GameController.js
+++ src/js/GameController.js
@@ -7,16 +7,25 @@ import Daemon from './Daemon';
 import Swordsman from './Swordsman';
 import Undead from './Undead';
 import { generateTeam } from './generators';
+import GamePlay from './GamePlay';
+import { userTypes } from './allowedTypes';
 
 export default class GameController {
   constructor(gamePlay, stateService) {
     this.gamePlay = gamePlay;
     this.stateService = stateService;
     this.boardSize = gamePlay.boardSize;
+    this.positions = [];
+    this.activeplayer = null;
   }
 
   init() {
+    // TODO: add event listeners to gamePlay events
+    // TODO: load saved stated from stateService
     this.gamePlay.drawUi(themes.prairie);
+    this.gamePlay.addCellClickListener(this.onCellClick.bind(this));
+    this.gamePlay.addCellEnterListener(this.onCellEnter.bind(this));
+
     console.log(`${'размер поля' + ': '}${this.boardSize}`);
     const userTeam = generateTeam([Magician, Bowman, Swordsman], 4, 2);
     const botTeam = generateTeam([Vampire, Daemon, Undead], 4, 2);
@@ -28,7 +37,7 @@ export default class GameController {
       if (positionedUserChar.position !== this.getPositionUser()) {
         positions.push(positionedUserChar);
       } else {
-        return this.getPositionUser();
+        this.getPositionUser();
       }
     }
 
@@ -37,16 +46,14 @@ export default class GameController {
       if (positionedBotChar.position !== this.getPositionBot()) {
         positions.push(positionedBotChar);
       } else {
-        return this.getPositionBot();
+        this.getPositionBot();
       }
     }
 
     console.log('fff', positions);
-
+    this.positions = positions;
     this.gamePlay.redrawPositions(positions);
     console.log(userTeam.members, botTeam.members);
-    // TODO: add event listeners to gamePlay events
-    // TODO: load saved stated from stateService
   }
 
   getPositionUser() {
@@ -69,10 +76,36 @@ export default class GameController {
 
   onCellClick(index) {
     // TODO: react to click
+    console.log(index);
+    console.log('positions', this.positions);
+    const positionChar = this.positions.find((item) => item.position === index);
+    console.log('positionChar', positionChar);
+
+    if (positionChar !== undefined) {
+      const player = userTypes.find((item) => positionChar.character instanceof item);
+      if (this.activeplayer !== null) {
+        this.gamePlay.deselectCell(this.activeplayer);
+      }
+      if (player) {
+        this.gamePlay.selectCell(index);
+        this.activeplayer = index;
+        console.log(this.activeplayer);
+      } else {
+        GamePlay.showError('Нельзя выбирать персонажа противника');
+      }
+    } else {
+      GamePlay.showError('Выберите своего персонажа');
+    }
   }
 
   onCellEnter(index) {
     // TODO: react to mouse enter
+
+    const positionChar = this.positions.find((item) => item.position === index);
+    if (positionChar !== undefined) {
+      console.log('наведение', positionChar);
+      this.gamePlay.showCellTooltip(`\u1F396${positionChar.character.level} \u2694${positionChar.character.attack} \u1F6E1${positionChar.character.defence} \u2764${positionChar.character.health}`, index);
+    }
   }
 
   onCellLeave(index) {
